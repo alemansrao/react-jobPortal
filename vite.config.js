b5606b5c955792a6aca8758a4e8fabe4601f0dff
alemansrao <sharathrao99@gmail.com>
2024-07-24 19:18:04 +0530

    Modified

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: process.env.VITE_API_URL,
+        target: 'https://json-server-two-kappa.vercel.app',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },

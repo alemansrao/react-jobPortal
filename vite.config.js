fc54701c9c0336188e2ab65d90626471bae2eef4
alemansrao <sharathrao99@gmail.com>
2024-07-24 18:44:14 +0530

    Modified to get rurl dynamically

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:4000",
+        target: process.env.VITE_API_URL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
